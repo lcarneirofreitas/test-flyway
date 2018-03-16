2c106ddf47f7b25fdd2b8b482f97aceeac2e9b8c
Leandro Freitas <leandro.freitas@teravoz.com.br>
2018-03-16 19:12:15 -0300

    changes

--- sql/V2__AsteriskServer_inserts.sql
+++ sql/V2__AsteriskServer_inserts.sql
@@ -1,4 +1,4 @@
-INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('bixira','10.0.0.90', '52.67.180.200', 0);
-INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('barros','10.0.0.166', '54.207.88.100', 0);
-INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('dias','10.0.0.237', '52.67.120.34', 1);
-INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('padre','10.0.0.167', '54.94.175.200', 0);
+INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('server1','10.0.0.90', '52.67.180.200', 0);
+INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('server2','10.0.0.91', '54.207.88.100', 0);
+INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('server3','10.0.0.92', '52.67.120.134', 1);
+INSERT INTO AsteriskServer (description, privateIP, publicIP, installationServer) VALUES ('server4','10.0.0.93', '54.94.175.200', 0);
